9acd20525f2e3bd8f8291a6b3eb190e7dda00994
Guilherme Falck do Carmo <122636771+GuilhermeFalck@users.noreply.github.com>
2024-04-18 18:18:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//1 script
+alert("Hello World!");
+//2 script
+let n1 = 9;
+let n2 = 2;
+alert(n1 + n2);
+//3 script
+let typeN = 90;
+if (typeof type === "number") {
+  alert("É um número");
+} else {
+  alert("Não é um número");
+}
+//4 script
+let typeS = "Olá";
+if (typeof typeS === "string") {
+  alert("É uma string");
+} else {
+  alert("Não é uma string");
+}
+//5,6,7 string
+alert(n1 - n2);
+alert(n1 * n2);
+alert(n1 / n2);
+
+let pairOdd = (n1 + n2) % 2;
+
+if (pairOdd === 0) {
+  alert("É um número par");
+} else {
+  alert("Não é um número par");
+}
+
+if (pairOdd != 0) {
+  alert("É um número impar");
+} else {
+  alert("Não é um número impar");
+}
